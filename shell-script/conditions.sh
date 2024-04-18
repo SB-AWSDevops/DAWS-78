bc48354560e3bc8c2357a4a67989446d2f707c7b
SurisettyGit <surisetty08@gmail.com>
2024-04-18 22:50:24 +0530

    added new script

--- shell-script/conditions.sh
+++ shell-script/conditions.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# num1=$1
-# num2=$2
-
-# if (( ($num1 > $num2 | bc -1) ))
-# then
-#  echo "$num1 is greater than $num2"
-# else
-#  echo "$num1 is less than $num2"
-# fi
-
-#!/bin/bash
-
 num1=$1
 num2=$2
 
-# Use bc for floating-point comparison
-if (( $(echo "$num1 > $num2" | bc -l) )); then
-  echo "$num1 is greater than $num2"
+if [ $num1 -gt $num2 ]
+then
+ echo "$num1 is greater than $num2"
 else
-  echo "$num1 is less than or equal to $num2"
+ echo "$num1 is less than $num2"
 fi
