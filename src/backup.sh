500a54f8a711e52277cf52da22d15b4577a8e6d6
SurisettyGit <surisetty08@gmail.com>
2024-04-19 22:02:00 +0530

    added new file and folder

--- src/backup.sh
+++ src/backup.sh
@@ -11,15 +11,18 @@ echo "Welcome to File Backup Script!!"
 
 #Validations for the src directory to check any loose files are there or not, if yes, we will copy to backup directory
 
-find "$src_dir" -maxdepth 1 -type f -exec cp -t "$backup_dir" {} \;
 
-if [ ! -d $backup ]
+
+if [ ! -d $backup_dir ]
  then
    echo "ERROR : DEST directory doesnot exists or it is not a directory"
 exit 5
-elif [ ! -w $sbackup ]
+elif [ ! -w $sbackup_dir ]
  then
    echo "ERROR : You dont have write permissions for this directory"
 exit 6
 fi
+
+find "$src_dir" -maxdepth 1 -type f -exec cp -t "$backup_dir" {} \;
+echo "copied files to backup foler successfullyy!!"
 
