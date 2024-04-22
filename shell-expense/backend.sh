c7e2984292b4800ebc57964bb96e03ed5eb28c68
SurisettyGit <surisetty08@gmail.com>
2024-04-22 21:35:54 +0530

    added new file and folder

--- shell-expense/backend.sh
+++ shell-expense/backend.sh
@@ -58,7 +58,7 @@ else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
-mkdir /app &>>$logfile
+mkdir -p /app &>>$logfile
 validate $? "app directory created"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$logfile
