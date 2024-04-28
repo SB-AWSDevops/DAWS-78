eb37f0a69144d97239a2e060491bbf55f841aea4
SurisettyGit <surisetty08@gmail.com>
2024-04-28 23:39:35 +0530

    added new file

---
+++ src/assignment/word-count.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#check the providence of filepath to read the file
+
+if [ $# -ne 1 ]
+ then
+   echo "please provide filename"
+   exit 1
+fi 
+
+#check if file exists
+
+if [ ! -f "$1" ]
+ then 
+   echo "file - $1 doesnot exists!!"
+   exit 2
+fi 
+
+#count the occurences and first most 5 used
+
+echo "Top 5 most used words in this txt file:"
+cat $1 | tr -s '[:space:]' '\n' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | sort -rn |head -n 5
