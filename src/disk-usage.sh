a3939f744662a421dfaff736ad0d38f9c040d5e3
SurisettyGit <surisetty08@gmail.com>
2024-04-28 21:14:02 +0530

    added new file

---
+++ src/disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep xfs)
+disk_threshold=5
+message=""
+
+while IFS= read -r line
+do 
+   usage=$(echo $line | awk -F " " '{print $6F}' | cut -d  "%" -f1)
+   folder=$(echo $line | awk -F " " '{print $NF}')
+   if [ $usage -ge $disk_threshold ]
+    then
+     message+="$folder is more than $disk_threshold, current usage is : $usage \n"
+   fi
+done <<< $disk_usage
+
+echo -e "Message : $message"
+
+echo "$message | mail -s "Disk Usage Alert" surisetty08@gmail.com
