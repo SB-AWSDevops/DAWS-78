3601f025bbd9b7cf6c40a3ac0994d187395365e9
SurisettyGit <surisetty08@gmail.com>
2024-04-29 12:20:53 +0530

    added new file

--- shell-expense/delete-oldfiles.sh
+++ shell-expense/delete-oldfiles.sh
@@ -6,12 +6,29 @@ src=/tmp/logs
 
 if [ -d $src ]
  then
-   echo "src exists - you can proceed further"
+   echo "src exists"
 else
    echo "src not exists!! pls check"
    exit 4
 fi
 
-files=find $src -name "*.log" -mtime +14
+files=$(find $src -name "*.log" -mtime +14)
 
 echo "files more than 14 days old are: $files"
+
+# for i in $files 
+# do
+# filename=$(echo $i | cut -d "/" -f4)
+# echo "filenames are : $filename"
+# rm "$i"
+# echo "$filename deleted"
+# done
+
+
+
+while IFS= read -r line
+do 
+echo "deleting file - $line"
+rm -rf $line
+done <<< $files
+
---
+++ src/assignment/zipfile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#check if arguments provided
+
+if [ $# -ne 2 ]
+ then 
+  echo "please provide src and destination folder properly!!"
+  exit 1
+fi
+
+#check if src and dest folders exists
+
+if [ ! -d $1 ]
+ then 
+  echo "src directory doesnot exists!!"
+ exit 1
+fi
+
+if [ ! -d $2 ]
+ then 
+  echo "dest directory not exists!!"
+  exit 3
+fi
+
+#create time stamp
+timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
+
+#create archive filename with timestamp
+archive_name="${$1##*/}_${timestamp}.tar.gz"
+
+#logic for compressing src directory 
+tar -czf "$2/$archive_name" -C "$(dirname "$1")" "$(basename "$1")"
+
+echo "backup created : $2/$archive_name"
